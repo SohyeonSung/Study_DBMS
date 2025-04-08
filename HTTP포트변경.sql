db89b226f9bf10ba4423ff312b7898fa21c0bd01
SohyeonSung <sasa547@naver.com>
2025-04-08 09:56:47 +0900

    Delete HTTP포트변경.sql

--- HTTP포트변경.sql
+++
@@ -1,9 +0,0 @@
--- SQL에서 한 줄 주석부호(--)
--- HTTP 포트 변경 : 8080 -> 8090
-SELECT DBMS_XDB.getHttpPort() from dual;
-SELECT DBMS_XDB.GETHTTPPORT() FROM DUAL;
-
--- HTTP 포트 8090 변경
-EXEC DBMS_XDB.setHttpPort(8090);
-
-SELECT DBMS_XDB.GETHTTPPORT() FROM DUAL;
