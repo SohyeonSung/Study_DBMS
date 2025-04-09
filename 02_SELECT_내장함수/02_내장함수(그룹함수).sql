c42e0582824c16a1839d6cfde48f7fef7c3b91a9
SohyeonSung <sasa547@naver.com>
2025-04-09 16:48:08 +0900

    Update 02_내장함수(그룹함수).sql

--- 02_SELECT_내장함수/02_내장함수(그룹함수).sql
+++ 02_SELECT_내장함수/02_내장함수(그룹함수).sql
@@ -7,3 +7,57 @@ AVG(컬럼명) : 평균값 구하기
 MAX(컬럼명) : 최대값 구하기
 MIN(컬럼명) : 최소값 구하기
 ******************************/
+
+
+
+SELECT * FROM BOOK;
+SELECT COUNT (*) FROM BOOK; --테이블 데이터 전체 건수 확인
+
+SELECT * FROM CUSTOMER;
+SELECT COUNT (*) FROM CUSTOMER;
+SELECT COUNT(CUSTID) FROM CUSTOMER;
+SELECT COUNT(NAME) FROM CUSTOMER;
+SELECT COUNT(PHONE) FROM CUSTOMER;
+----------------
+--PHONE 컬럼 값이 NULL 인 데이터 건수
+SELECT COUNT(*) FROM CUSTOMER WHERE PHONE IS NULL; -- 1건
+--PHONE 컬럼 값이 NULL이 아닌 데이터 건수
+SELECT COUNT(*) FROM CUSTOMER WHERE PHONE IS NULL; -- 4건
+SELECT COUNT(PHONE) FROM CUSTOMER; --4건
+
+------------------------------------------
+--SUM(컬럼명) : 합계값 구하기
+SELECT * FROM BOOK;
+SELECT SUM(PRICE) FROM BOOK; --144500
+
+--(실습)대한미디어, 이상미디어 출판사에서 출판한 모든 책 금액 합계
+SELECT SUM(PRICE)
+FROM BOOK
+WHERE PUBLISHER IN ('대한미디어', '이상미디어');
+
+
+-- AVG(컬럼명) : 평균값 구하기
+---(실습) 대한미디어, 이상미디어 출판사에서 출판한 모든 책 금액 평균
+SELECT AVG(PRICE)
+FROM BOOK
+WHERE PUBLISHER IN ('대한미디어', '이상미디어');
+
+
+-- MAX(컬럼명) : 최대값 구하기
+-- MIN(컬럼명) : 최소값 구하기
+-- (실습) 굿스포츠 출판한 책중 최고가, 최저가 구하기
+SELECT MAX(PRICE)
+FROM BOOK
+WHERE PUBLISHER = '굿스포츠';
+
+SELECT MIN(PRICE)
+FROM BOOK
+WHERE PUBLISHER = '굿스포츠';
+
+-- (실습) 책제목(도서명)에 대해 MIN, MAX 값은 무엇인지 확인
+SELECT MIN(BOOKNAME), MAX(BOOKNAME) FROM BOOK;
+SELECT * FROM BOOK ORDER BY BOOKNAME;
+
+--(실습)출판된 책 갯수(권수), 합계금액, 평균금액, 가장비싼책값, 가장싼책값
+SELECT COUNT(*), SUM(PRICE), AVG(PRICE), MAX(PRICE), MIN(PRICE)
+FROM BOOK;
